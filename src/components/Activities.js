0c76f95b4ab2120411b3f7c4fec1f0608772a288
David Chaney <david.ray.chaney@gmail.com>
2023-03-29 16:19:46 -0500

    Add getActivities API function

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -5,3 +5,44 @@ function Activities() {
 }
 
 export default Activities;
+
+// Sample code used to test getActivities() function in /api/index.js
+// Feel free to use this starter-code.
+
+// import { useState, useEffect } from "react";
+// import getActivities from "./components/api";
+
+
+// function App() {  
+//   const [activities, setActivities] = useState([]);  
+  
+//   useEffect(()=>{
+//     async function getActs(){
+//       try{
+//         const response = await getActivities()
+//         setActivities(response)
+//       } catch(error) {
+//         console.error(error)
+//       }
+//     }
+//     getActs();
+//   },[]);
+  
+
+//   return(
+//     <div>
+//       <h1>Fitness Track</h1>
+//       {
+//         activities.map((activity)=>{
+//           return(
+//             <div>
+//               <p>Activity ID: {activity.id}</p>
+//               <p>Activity Name: {activity.name}</p>
+//               <p>Activity Description: {activity.description}</p>
+//             </div>
+//           )
+//         })
+//       }
+//     </div>
+//   )
+// }
---
+++ src/components/api/index.js
@@ -0,0 +1,12 @@
+async function getActivities(){
+  const APIURL = "http://fitnesstrac-kr.herokuapp.com/api/activities"
+  try {
+    const response = await fetch(APIURL)
+    const results = await response.json();
+    return results;
+  } catch(error) {
+    console.error (error);
+  }
+}
+
+export default getActivities;
