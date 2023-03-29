c93f8c5f1e1322d748937e9d116d70f4d96c0457
clopez-0 <clopez0@gmail.com>
2023-03-29 14:46:51 -0500

    Login Function Complete

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,6 +1,44 @@
-function Login() {
-  return(
-    <div>Login</div>
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+
+export function Login({setToken}) {
+  const [userName, setUserName]= useState('')
+  const [password, setPassword]= useState('')
+  const [verifiedUser, setVerifiedUser]= useState('')
+  const navigate = useNavigate()
+
+  async function loginUser(){
+    event.preventDefault()
+    try{
+      const response = await fetch ('https://fitnesstrac-kr.herokuapp.com/api/2211FTB-CT-WEB-PT/users/login',{
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          username: userName,
+          password: password
+        })
+      });
+      let result = await response.json()
+      setToken(result.data.token)
+      localStorage.setItem("token", result.data.token)
+      //If we choose to take this route for setVerifiedUser
+      setVerifiedUser(userName)
+      navigate("/MyRoutines")
+    }catch(error){
+      console.log("There was an error logging in", error)
+    }
+
+
+  }
+
+  return (
+    <form onSubmit={(event) => loginUser(event)}>
+      <input type="text" value={userName} onChange={(event)=>setUserName (event.target.value)} placeholder="Username"></input>
+      <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)} placeholder="Password"></input>
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
