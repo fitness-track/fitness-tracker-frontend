008b914f4e408ecae3f759ad9c5862cf740534fa
David Chaney <david.ray.chaney@gmail.com>
2023-03-31 16:27:23 -0500

    add unformatted card

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -7,7 +7,6 @@ export default function Routines({token}) {
       useEffect(()=>{
       async function getRoutines(){
         const response = await getRoutinesAPI()
-        console.log("Hello!");
         setRoutines(response)
       }
       getRoutines();
@@ -15,29 +14,58 @@ export default function Routines({token}) {
 
   return(
         <div>
-          <h1>Fitness Track</h1>
+          <h1>Routines</h1>
           {
             routines?.map((routine)=>{
               return (
-                <div>
-                  <p>Routine ID: {routine.id}</p>
-                  <p>Creator ID: {routine.creatorId}</p>
-                  <p>Routine Name: {routine.name}</p>
-                  <p>Routine Goal: {routine.goal}</p>
-                  <p>Routine creatorName: {routine.creatorName}</p>
-                  { 
-                    routine.activities.map((routineActivity)=>{
-                      return (
-                        <div>
-                          <p>Activity ID: {routineActivity.id}</p>
-                          <p>Activity Name: {routineActivity.name}</p>
-                          <p>Activity Description: {routineActivity.description}</p>
-                          <p>Activity Duration: {routineActivity.duration}</p>
-                          <p>Activity Count.: {routineActivity.count}</p>
-                        </div>
-                      )
-                    })
-                  }
+                <div className="card routinesCard text-bg-primary mb-3">
+                  {/* <p>Routine ID: {routine.id}</p> */}
+                  {/* <p>Creator ID: {routine.creatorId}</p> */}
+                  <p>Routine: {routine.name}</p>
+                  <p>Goal: {routine.goal}</p>
+                  <p>creatorName: {routine.creatorName}</p>
+                  <p>Number of activities: {routine.activities.length}</p>
+                  <div className="card activityCard text-bg-secondary mb-3">
+                    <div id={"rId" + routine.id} className="carousel slide">
+                      <div className="carousel-inner">
+                        {
+                          routine.activities.map((index, routineActivity)=>{
+                            console.log(routineActivity)
+                            console.log(index)
+                            return(
+                              index === 0?
+                                <div className="carousel-item active">
+                                  <div className="d-block w-100">
+                                    <p>Activity ID: {routineActivity.id}</p>
+                                    <p>Activity Name: {routineActivity.name}</p>
+                                    <p>Activity Description: {routineActivity.description}</p>
+                                    <p>Activity Duration: {routineActivity.duration}</p>
+                                    <p>Activity Count.: {routineActivity.count}</p>
+                                   </div>
+                                </div>:
+                                <div className="carousel-item">
+                                  <div className="d-block w-100">
+                                    <p>Activity ID: {routineActivity.id}</p>
+                                    <p>Activity Name: {routineActivity.name}</p>
+                                    <p>Activity Description: {routineActivity.description}</p>
+                                    <p>Activity Duration: {routineActivity.duration}</p>
+                                    <p>Activity Count.: {routineActivity.count}</p>
+                                  </div>                            
+                                </div>
+                            )
+                          })
+                        }
+                      </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target={"rId" + routine.id} data-bs-slide="prev">
+                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target={"rId" + routine.id} data-bs-slide="next">
+                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                  </div>
                 </div>
               )
             })
