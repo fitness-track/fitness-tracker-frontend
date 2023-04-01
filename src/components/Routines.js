d2b395c50825d0529831abf68613133c905600a9
David Chaney <david.ray.chaney@gmail.com>
2023-04-01 08:31:27 -0500

    add accordion to cards

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getRoutinesAPI} from "../api"
 
 export default function Routines({token}) {
-  const [routines, setRoutines] = useState([]);  
+  const [routines, setRoutines] = useState([]);
 
       useEffect(()=>{
       async function getRoutines(){
@@ -18,52 +18,39 @@ export default function Routines({token}) {
           {
             routines?.map((routine)=>{
               return (
-                <div className="card routinesCard text-bg-primary mb-3">
+                <div className="card routinesCard text-bg-primary mb-3" key={"rId" + routine.id}>
                   {/* <p>Routine ID: {routine.id}</p> */}
                   {/* <p>Creator ID: {routine.creatorId}</p> */}
                   <p>Routine: {routine.name}</p>
                   <p>Goal: {routine.goal}</p>
-                  <p>creatorName: {routine.creatorName}</p>
-                  <p>Number of activities: {routine.activities.length}</p>
+                  <p>Created By: {routine.creatorName}</p>
+                  {
+                    routine.activities.length>0?<p>Activities:</p>:<p>No activities assigned to this routine</p>
+                  }
+                  
                   <div className="card activityCard text-bg-secondary mb-3">
-                    <div id={"rId" + routine.id} className="carousel slide">
-                      <div className="carousel-inner">
-                        {
-                          routine.activities.map((index, routineActivity)=>{
-                            console.log(routineActivity)
-                            console.log(index)
-                            return(
-                              index === 0?
-                                <div className="carousel-item active">
-                                  <div className="d-block w-100">
-                                    <p>Activity ID: {routineActivity.id}</p>
-                                    <p>Activity Name: {routineActivity.name}</p>
-                                    <p>Activity Description: {routineActivity.description}</p>
+                    <div id={"rId" + routine.id} className="accordion">
+                      {
+                        routine.activities.map((routineActivity, index)=>{
+                          return(                                                               
+                              <div className="accordion-item" key={"rId" + routine.id + routineActivity.name}>
+                                <h2 className="accordion-header">
+                                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#rId" + routine.id + routineActivity.id} aria-expanded="true" aria-controls={"rId" + routine.id + routineActivity.id}>
+                                    {/* <p>Activity ID: {routineActivity.id}</p> */}
+                                    Activity Name: {routineActivity.name}
+                                  </button>                            
+                                </h2>
+                                <div id={"rId" + routine.id + routineActivity.id} className="accordion-collapse collapse" data-bs-parent={"#rId" + routine.id}>
+                                  <div className="accordion-body">
+                                    <p><strong>Activity Description: {routineActivity.description}</strong></p>
                                     <p>Activity Duration: {routineActivity.duration}</p>
                                     <p>Activity Count.: {routineActivity.count}</p>
-                                   </div>
-                                </div>:
-                                <div className="carousel-item">
-                                  <div className="d-block w-100">
-                                    <p>Activity ID: {routineActivity.id}</p>
-                                    <p>Activity Name: {routineActivity.name}</p>
-                                    <p>Activity Description: {routineActivity.description}</p>
-                                    <p>Activity Duration: {routineActivity.duration}</p>
-                                    <p>Activity Count.: {routineActivity.count}</p>
-                                  </div>                            
+                                  </div>
                                 </div>
-                            )
-                          })
-                        }
-                      </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target={"rId" + routine.id} data-bs-slide="prev">
-                          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target={"rId" + routine.id} data-bs-slide="next">
-                          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span className="visually-hidden">Next</span>
-                        </button>
+                              </div>
+                          )
+                        })
+                      }
                     </div>
                   </div>
                 </div>
