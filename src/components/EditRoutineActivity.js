73a0af5cf202f904a73d0fce913c998caa481a38
clopez-0 <clopez0@gmail.com>
2023-04-07 09:43:07 -0500

    All Edit/Delete functions working

--- src/components/EditRoutineActivity.js
+++ src/components/EditRoutineActivity.js
@@ -1,23 +1,20 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom"
-
 import {patchRoutineActivityByIdAPI, deleteRoutineActivityById} from "../api"
 
-
-
 export default function EditRoutineActivity({token}) {
-    const {routineActivityId, activityDuration, activityCount} = useParams();
-    const[duration, setDuration] = useState('');
-    const[count, setCount] = useState('');
+  const {routineActivityId, activityDuration, activityCount} = useParams();
+  const[duration, setDuration] = useState('');
+  const[count, setCount] = useState('');
 
-    async function editRoutineActivity(event){
-        event.preventDefault()
-        console.log(routineActivityId,duration, count)
-        const results = await patchRoutineActivityByIdAPI(token, routineActivityId, count, duration)
-        console.log(results)
-      }
+  async function editRoutineActivity(event){
+    event.preventDefault()
+    console.log(routineActivityId,duration, count)
+    const results = await patchRoutineActivityByIdAPI(token, routineActivityId, count, duration)
+    console.log(results)
+  }
 
-return(
+  return(
     <>
     {console.log(routineActivityId, activityDuration, activityCount)}
       <div>
@@ -26,26 +23,20 @@ return(
         <div>{activityCount}</div>
       </div>
 
-<div>
-<h3>Edit Routine Activity</h3>
-<form className='form' onSubmit={editRoutineActivity}>
-    <input className="formField" type="text" value={duration} onChange={(event)=>setDuration(event.target.value)} placeholder={activityDuration}></input>
-    <input className="formField" type="text" value={count} onChange={(event)=>setCount(event.target.value)} placeholder={activityCount}></input>
-    <button id='submitButton' type="submit">Edit Routine Activity</button>
-</form>
-<button
-                      onClick={() => {
-                        // setRoutineId(routine.id);
-                        deleteRoutineActivityById(token,routineActivityId);
-                        console.log("RoutineActivity", routineActivityId, "deleted")
-                      }}
-                    >
-                      Delete Routine Actvity
-                </button>
-</div>
-      
-    
-</>
-      
-      )
+    <div>
+    <h3>Edit Routine Activity</h3>
+    <form className='form' onSubmit={editRoutineActivity}>
+      <input className="formField" type="text" value={duration} onChange={(event)=>setDuration(event.target.value)} placeholder={activityDuration}></input>
+      <input className="formField" type="text" value={count} onChange={(event)=>setCount(event.target.value)} placeholder={activityCount}></input>
+      <button id='submitButton' type="submit">Edit Routine Activity</button>
+    </form>
+    <button onClick={() => {
+      deleteRoutineActivityById(token,routineActivityId);
+      console.log("RoutineActivity", routineActivityId, "deleted")
+    }}>
+      Delete Routine Activity
+    </button>
+    </div>    
+    </>
+  )
 } 
