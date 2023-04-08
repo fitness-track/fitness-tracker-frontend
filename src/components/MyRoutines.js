9de0f42c69d125740648a81bf4438c849331768b
emmajanephilips <phillipsemmajane01@gmail.com>
2023-04-08 16:30:11 -0500

    some styling touchups

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -3,7 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 import { getUsernameRoutines, postRoutineAPI, getActivitiesAPI} from "../api"
 import Loading from "./Loading";
-import './Routines.css';
+import './MyRoutines.css';
 
 export default function MyRoutines({token, username, setFooterMessage}) {
   const [routines, setRoutines] = useState([]);
@@ -59,14 +59,18 @@ export default function MyRoutines({token, username, setFooterMessage}) {
   
       {routines?.map((routine)=>{
         return (
+          
             <div className="col">
-              <div className="card routinesCard text-bg-primary" key={"rId" + routine.id}>
+              <div className="routinesCard" key={"rId" + routine.id}>
                 <p>Routine ID: {routine.id}</p>
                 <div className="card-body">
                   <h3>{routine.name}</h3>
                   <h5>{routine.goal}</h5>
+                  {
+                    routine.activities.length>0?<p>This routine has <strong>{routine.activities.length}</strong> activities:</p>:<p>No activities assigned to this routine</p>
+                  }
                   <Link to={`../../EditRoutine/${routine.id}/${routine.name}/${routine.goal}`}>
-                    <button type="button" className="btn btn-primary" data-toggle="tooltip" title="Edit Routine"><i className="bi bi-scissors"></i></button>
+                  <button type="button" className="button" data-toggle="tooltip" ><i className="bi-scissors"></i></button>
                   </Link>
               
                   <div className="card activityCard text-bg-secondary mb-3 overflow-auto">
@@ -77,18 +81,18 @@ export default function MyRoutines({token, username, setFooterMessage}) {
                       <div className="accordion-item" key={"rId" + routine.id + routineActivity.name}>
                         <h2 className="accordion-header">
                         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#rId" + routine.id + routineActivity.id} aria-expanded="true" aria-controls={"rId" + routine.id + routineActivity.id}>
-                          Activity Name: {routineActivity.name}
+                        {routineActivity.name}
                         </button>
                         </h2>
                         <div id={"rId" + routine.id + routineActivity.id} className="accordion-collapse collapse" data-bs-parent={"#rId" + routine.id}>
                           <div className="accordion-body">
-                            <p><strong>Activity Description: {routineActivity.description}</strong></p>
-                            <p>Routine Activity ID: {routineActivity.routineActivityId}</p>
-                            <p>Activity Duration: {routineActivity.duration}</p>
-                            <p>Activity Count.: {routineActivity.count}</p>
+                            <p><i className="bi bi-fire"></i>{routineActivity.description}</p>
+                            <p><i className="bi-stopwatch-fill"></i> {routineActivity.duration}</p>
+                            <p><i className="bi bi-repeat"></i>{routineActivity.count}</p>
+                            <p>ID: {routineActivity.routineActivityId}</p>
                           </div>
                           <Link to={`../../EditRoutineActivity/${routineActivity.routineActivityId}/${routineActivity.duration}/${routineActivity.count}`}>
-                            <button type="button" className="btn btn-primary" data-toggle="tooltip" title="Edit Routine"><i className="bi bi-scissors"></i></button>
+                            <button type="button" className="button" data-toggle="tooltip" ><i className="bi-scissors"></i></button>
                           </Link>
                         </div>
 
@@ -107,16 +111,20 @@ export default function MyRoutines({token, username, setFooterMessage}) {
           </div>
         </div>
       </div>
-    <div>
-    <h3>Create a New Routine</h3>
-    <form className='form' onSubmit={postRoutine}>
-        <input className="formField" type="text" value={name} onChange={(event)=>setName(event.target.value)} placeholder="Name"></input>
-        <input className="formField" type="text" value={goal} onChange={(event)=>setGoal(event.target.value)} placeholder="Goal"></input>
-        Is Public?
-        <input id='checkbox' type="checkbox" value={isPublic} onChange={()=>setIsPublic(!isPublic)} placeholder="Is this a public routine?"></input>
-        <button id='submitButton' type="submit">Submit Post</button>
-    </form>
-    </div>
+      <div className="col">
+        <form className="centered-form card p-2 container" onSubmit={postRoutine}>
+          <h4>Add a new routine:</h4>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" id="activityNameForm"  value={name} onChange={(event)=>setName(event.target.value)} />
+            <label htmlFor="activityNameForm">Name:</label>
+          </div>
+          <div className="form-floating mb-3">
+            <textarea type="text" className="form-control" id="descForm" value={goal} onChange={(event)=>setGoal(event.target.value)}/>
+            <label htmlFor="descForm">Goal:</label>
+          </div>
+          <button className="btn btn-secondary" type="submit"><i className="bi bi-bookmark-plus"></i> Add New Routine</button>
+        </form>
+      </div>
 
     </section>:null
   )
